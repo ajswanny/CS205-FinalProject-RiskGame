0e97bba9ac8dbe519b1a656a281dc634def93c53
MBP15-GithubDesktop <alexanderjswanson@icloud.com>
2019-03-06 19:04:29 -0500

    Added Controllers for the Menu and Game Scenes.

--- src/risk/Game.java
+++ src/risk/Game.java
@@ -1,12 +1,31 @@
 package risk;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
+    /** Height and Width of window. */
+    public static final int HEIGHT = 1000, WIDTH = 800;
+
+    private Scene primaryScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        primaryScene = new Scene(new Pane());
+
+        primaryStage.setTitle("Risk");
+
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
 
     }
 
