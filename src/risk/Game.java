994665b479b2f053d669599afb666b93f9235e3b
MBP15-GithubDesktop <alexanderjswanson@icloud.com>
2019-03-07 09:41:31 -0500

    Added some documentation; skeleton for Game main class; tester class for Game.

--- src/risk/Game.java
+++ src/risk/Game.java
@@ -1,25 +1,73 @@
 package risk;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import risk.controller.GameSceneController;
+import risk.controller.MenuSceneController;
 
 public class Game extends Application {
 
     /** Height and Width of window. */
     public static final int HEIGHT = 1000, WIDTH = 800;
 
+    /** The Game-App's primary Scene. */
     private Scene primaryScene;
 
+    /** The Controller object for the Menu Scene. */
+    private MenuSceneController menuSceneController;
+
+    /** The root View of the Menu Scene.*/
+    private AnchorPane menuSceneRootView;
+
+    /** The Controller object for the Game Scene. */
+    private GameSceneController gameSceneController;
+
+    /** The root View of the Menu Scene. */
+    private AnchorPane gameSceneRootView;
+
+    /**
+     * Starts the Game GUI.
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        generateFxmlLoaders();
+
         primaryScene = new Scene(new Pane());
+        primaryScene.setRoot(menuSceneRootView);
 
         primaryStage.setTitle("Risk");
+        primaryStage.setScene(primaryScene);
 
         primaryStage.show();
+
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Thanks for playing Risk!");
+    }
+
+    private void generateFxmlLoaders() throws Exception {
+
+        FXMLLoader fxmlLoader;
+
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("fxml/MenuSceneController.fxml"));
+        menuSceneController = fxmlLoader.getController();
+        menuSceneRootView = fxmlLoader.load();
+
+//        fxmlLoader = new FXMLLoader();
+//        fxmlLoader.setLocation(getClass().getResource("fxml/GameSceneController"));
+//        gameSceneController = fxmlLoader.getController();
+//        gameSceneRootView = fxmlLoader.load();
 
     }
 
--- src/risk/controller/MenuSceneController.java
+++ src/risk/controller/MenuSceneController.java
@@ -1,14 +1,29 @@
 package risk.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuSceneController implements Initializable {
 
+    @FXML
+    public AnchorPane root;
+
+    @FXML
+    public Label testLabel;
+
+    public MenuSceneController() {
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        testLabel.setText("Hello");
+
     }
 
 }
