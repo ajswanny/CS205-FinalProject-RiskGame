810622d40acba99d37b689eae489bd50e4cba477
daleblarie <DaleBLarie@gmail.com>
2019-04-17 16:37:55 -0400

    updated methods to player class

--- src/risk/java/Player.java
+++ src/risk/java/Player.java
@@ -8,4 +8,30 @@ public class Player {
     private int turnValue;
     private int numOfArmies;
     private int numOfNewArmies;
+    private boolean endOfTurn;
+
+    public void setNumOfNewArmies(int numOfNewArmies) {
+        this.numOfNewArmies = numOfNewArmies;
+    }
+
+    public int getNumOfNewArmies(){
+        return this.numOfNewArmies;
+    }
+
+    public void setEndOfTurn(boolean end){
+        this.endOfTurn = end;
+    }
+
+    public boolean getEndOfTurn(){
+        return this.endOfTurn;
+    }
+
+    public void deployArmies(Territory territory, int numArmiesToAdd){
+        int armiesLeft = getNumOfNewArmies();
+        if (numArmiesToAdd <= getNumOfNewArmies()){
+            territory.numOfArmies += numArmiesToAdd;
+            armiesLeft -= numArmiesToAdd;
+            setNumOfNewArmies(armiesLeft);
+        }
+    }
 }
